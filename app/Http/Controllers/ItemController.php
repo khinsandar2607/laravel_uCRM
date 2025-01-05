172bf94e3d555bdc50d3fb042b47d17716bf34ef
khinsandar2607 <106940057+khinsandar2607@users.noreply.github.com>
2025-01-05 20:42:00 +0900

    sec03_items_商品管理機能の実装 Item Management Implementation ase enter the commit message for your changes. Lines starting

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StoreItemRequest;
 use App\Http\Requests\UpdateItemRequest;
 use App\Models\Item;
+use Inertia\Inertia;
 
 class ItemController extends Controller
 {
@@ -15,7 +16,14 @@ class ItemController extends Controller
      */
     public function index()
     {
-        //
+        $items = 
+        Item::select('id', 'name', 'price', 'is_selling')
+        // ->orderBy('id', 'desc')
+        ->get();
+
+        return Inertia::render('Items/Index', [
+            'items' => $items,
+        ]);
     }
 
     /**
@@ -25,7 +33,7 @@ class ItemController extends Controller
      */
     public function create()
     {
-        //
+        return Inertia::render('Items/Create');
     }
 
     /**
@@ -36,7 +44,17 @@ class ItemController extends Controller
      */
     public function store(StoreItemRequest $request)
     {
-        //
+        Item::create([
+            'name' => $request->name,
+            'memo' => $request->memo,
+            'price' => $request->price,
+        ]);
+
+        return to_route('items.index')
+        ->with([
+            'message' => '登録しました',
+            'status' => 'success',
+        ]);
     }
 
     /**
@@ -47,9 +65,11 @@ class ItemController extends Controller
      */
     public function show(Item $item)
     {
-        //
+        // dd($item);
+        return Inertia::render('Items/Show', [
+            'item' => $item,
+        ]);
     }
-
     /**
      * Show the form for editing the specified resource.
      *
@@ -58,7 +78,9 @@ class ItemController extends Controller
      */
     public function edit(Item $item)
     {
-        //
+        return Inertia::render('Items/Edit', [
+            'item' => $item,
+        ]);
     }
 
     /**
@@ -70,7 +92,14 @@ class ItemController extends Controller
      */
     public function update(UpdateItemRequest $request, Item $item)
     {
-        //
+        // dd($item->name, $request->name);
+        $item->name = $request->name;
+        $item->memo = $request->memo;
+        $item->price = $request->price;
+        $item->is_selling = $request->is_selling;
+        $item->save();
+
+        return to_route('items.index');
     }
 
     /**
@@ -81,6 +110,12 @@ class ItemController extends Controller
      */
     public function destroy(Item $item)
     {
-        //
+        $item->delete();
+
+        return to_route('items.index')
+        ->with([
+            'message' => '削除しました',
+            'status' => 'danger',
+        ]);
     }
 }
--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+// use Illuminate\Support\Facades\Hash;
 
 class ItemSeeder extends Seeder
 {
@@ -14,6 +16,27 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        // 
+        DB::table('items')->insert([
+            [
+            'name' => 'Hair Cut',
+            'memo' => 'Cut your hair', 
+            'price' => 6000,
+            ],
+            [
+            'name' => 'Hair Color',
+            'memo' => 'Color your hair', 
+            'price' => 10000,
+            ],
+            [
+            'name' => 'Nail Cut',
+            'memo' => 'Cut your nails', 
+            'price' => 5000,
+            ],
+            [
+            'name' => 'Nail Color',
+            'memo' => 'Color your nails', 
+            'price' => 6000,
+            ],
+        ]);
     }
 }
